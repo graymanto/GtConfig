298fb2c3d512ebb054b22af6573d10a8db896d5c
Graham Thomas <githomas@hotmail.com>
2017-05-11 22:16:12 +0200

    More generic nvm config

--- .zsh/langsettings.zsh
+++ .zsh/langsettings.zsh
@@ -3,7 +3,5 @@ then
 	export PATH="$PATH:$GOPATH/bin"
 fi
 
-if [[ $IS_MAC -eq 1  ]]; then
-	export NVM_DIR=~/.nvm
-	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-fi
+export NVM_DIR=~/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
